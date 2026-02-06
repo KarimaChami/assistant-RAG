3f2c9d90d01729e7d7ef2004e1475d51875e4e5a
KarimaChami <karimachami24@gmail.com>
2026-02-06 14:02:04 +0100

    fix testapi for CI

--- src/api/db.py
+++ src/api/db.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
 
 
-DATABASE_URL = "postgresql://postgres:root1234@localhost:5432/it_assistant"
-
+# DATABASE_URL = "postgresql://postgres:root1234@localhost:5432/it_assistant"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./test.db"
+)
 engine = create_engine(DATABASE_URL,connect_args={"options": "-c client_encoding=utf8"})
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
